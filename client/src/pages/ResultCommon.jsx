9858cfcbe601a5eff47d57e3007b61994c2110ef
hwang-jy <hwangjukyong@gmail.com>
2026-03-08 05:40:31 +0900

    fix api url

--- client/src/pages/ResultCommon.jsx
+++ client/src/pages/ResultCommon.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import axios from "axios";
 import { Link } from "react-router-dom";
 import "./ResultCommon.css";
 import BeforeAfter from "./BeforeAfter";
